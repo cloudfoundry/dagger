9dd5da1e0b6c63c9c1b197b433a5bea6fa9574c3
Jackson Feeny <jacksonfeeny@gmail.com>
2018-09-27 10:17:28 -0400

    Initial commit [#160699500]
    Co-authored-by: Danny Joyce <djoyce@pivotal.io>

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/cloudfoundry/dagger
+
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/buildpack/libbuildpack v0.0.0-20180917215559-e0a1f1ae6c05
+)
---
+++ util.go
@@ -0,0 +1,33 @@
+package dagger
+
+import (
+	"io"
+	"math/rand"
+	"os"
+)
+
+func copyFile(from, to string) error {
+	source, err := os.Open(from)
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(to)
+	if err != nil {
+		return err
+	}
+	defer destination.Close()
+
+	_, err = io.Copy(destination, source)
+	return err
+}
+
+func randomString(n int) string {
+	letterRunes := []rune("abcdefghijklmnopqrstuvwxyz")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
